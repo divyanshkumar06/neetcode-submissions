624586234291cc02adb0759745965db1aaf074c7
divyanshkumar06 <divyanshkumar192@gmail.com>
2026-09-07 19:06:30 +0530

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> v;
+        int i = 0;
+        int j = nums.size() - 1;
+        while (i < j) {
+            if (nums[i] + nums[j] == target) {
+                v.push_back(i+1);
+                v.push_back(j+1);
+            }
+
+            if (nums[i] + nums[j] > target) {
+                j--;
+            } else {
+                i++;
+            }
+        }
+        return v;
+    }
+};
